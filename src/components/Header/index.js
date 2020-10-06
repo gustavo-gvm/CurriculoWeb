fc85f660d0f5c2d60231ecd03dc022d852151813
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-10-06 15:14:32 -0300

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <nav id="navbar">
+    Navbar
+  </nav>
+);
+
+export default Header;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => (
+  <h2>Configuração Inicial</h2>
+);
+
+export default Main;
