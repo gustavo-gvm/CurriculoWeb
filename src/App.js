1e13a082de816d1ec3496b228f42032c9b857e01
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-10-07 14:17:36 -0300

    Navbar adicionado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header/index';
 import Main from './pages/Main';
+import './styles.css';
 
 const App = () => (
-    <div>
+    <div id="page-top">
       <Header/>
       <Main/>
+      <Footer/>
     </div>
 );
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import './styles.css';
 
 const Header = () => (
-  <nav id="navbar">
-    Navbar
-  </nav>
+  <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top" id="navbar">
+  <Navbar.Brand href="#page-top">GVM</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="navbar-nav">
+      <Nav.Link href="#perfil">Perfil</Nav.Link>
+      <Nav.Link href="#graduacao">Graduacao</Nav.Link>
+      <Nav.Link href="#certificados">Certificados</Nav.Link>
+      <Nav.Link href="#experiencia">Experiencia</Nav.Link>
+      <Nav.Link href="#contato">Contato</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 );
 
 export default Header;
